1ac7ebc34916c995c71c5d4524d729e557b01f5f
Emine Altuntaş <emineealtuntas@gmail.com>
2022-10-04 23:06:41 +0300

    Database contexde kaldık

--- SMStore.Entities/Post.cs
+++ SMStore.Entities/Post.cs
@@ -2,7 +2,7 @@
 
 namespace SMStore.Entities
 {
-    public class Category : IEntity
+    public class Post : IEntity
     {
         public int Id { get; set; }
         [Display(Name = "Adı"), Required(ErrorMessage = "{0} Alanı Boş Geçilemez!")]
@@ -13,20 +13,7 @@ namespace SMStore.Entities
         public string? Image { get; set; }
         [Display(Name = "Aktif?")]
         public bool IsActive { get; set; }
-        [Display(Name = "Üst Menü?")]
-        public bool IsTopMenu { get; set; }
-        [Display(Name = "Üst Kategori")]
-        public int ParentId { get; set; }
-        [Display(Name = "Sıra No")]
-        public int OrderNo { get; set; }
         [Display(Name = "Eklenme Tarihi"), ScaffoldColumn(false)]
         public DateTime? CreateDate { get; set; } = DateTime.Now;
-        public virtual ICollection<Product> Products { get; set; } // 1 kategorinin 1 den çok ürünü olabilir. 1 e çok ilişki kurduk
-        public Category()
-        {
-            Products = new List<Product>();
-        }
-
     }
 }
-
